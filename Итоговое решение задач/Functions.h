2000b594a59b87808533ce340b2bfce8aae4d00d
Б - 5 < - 5@DESKTOP-EOJTJK3>
2023-04-25 21:02:50 +0300

    Добавьте файлы проекта.

---
+++ Итоговое решение задач/Functions.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#ifndef _FUNCTIONS_H_
+#define	_FUNCTIONS_H_
+#include <iostream>
+#include <string>
+
+// UTF-8
+
+// Выделение памяти под двумерный динамический массив
+template <typename T>
+void mx_memory(T**& pointer, int rows, int cols) {
+	pointer = new T* [rows];
+	for (int i{}; i < rows; i++)
+		pointer[i] = new T[cols]{};
+}
+
+// Освобождение динамической памяти 
+template <typename T>
+void clear_mx_memory(T**& pointer, int rows) {
+	for (int i{}; i < rows; i++)
+		delete[]pointer[i];
+	delete[]pointer;
+	pointer = nullptr;
+}
+
+// Возврат местоположения массива, состоящего из олоожительных элементов, переданных массивов
+template <typename T>
+T* pos_mem(T arr1[], const int s1, T arr2[], const int s2, int& var) {
+	T* tmp = new T[s1 + s2];
+	var = 0;
+	for (int i{}; i < s1; i++)
+		if (arr1[i] > 0)
+			tmp[var++] = arr1[i];
+	for (int i{}; i < s2; i++)
+		if (arr2[i] > 0)
+			tmp[var++] = arr2[i];
+
+	T* result = new T[var];
+	for (int i{}; i < var; i++)
+		result[i] = tmp[i];
+
+	delete[] tmp;
+	return result;
+}
+
+// Запись названия числа в файл
+bool number_name(int num, std::string file_path);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // _FUNCTIONS_H_
---
+++ Итоговое решение задач/main.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include "Functions.h"
+
+// Кодировка UTF-8
+
+// Заполнение массива
+template <typename T>
+void fill(T arr[], const int s, int left, int right) {
+	srand(time(NULL));
+	for (int i{}; i < s; i++)
+		arr[i] = rand() % (right - left) + left;
+}
+
+// Вывод
+template <typename T>
+void show(T arr[], const int s) {
+	std::cout << "[";
+	for (int i{}; i < s; i++)
+		std::cout << arr[i] << ", ";
+	std::cout << "\b\b]\n";
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n,m,s2_3;
+
+	// Задача 1. Выделение памяти под двумерный массив
+	/*
+	std::cout << "Задача 1.\nВведите количество рядов -> ";
+	std::cin >> n;
+	std::cout << "Введите количество столбцов -> ";
+	std::cin >> m;
+	int** mx;
+	mx_memory(mx, n, m);
+
+	std::cout << "Сгенерированный массив:\n";
+	for (int i{}; i < n; i++) {
+		for (int j{}; j < m; j++)
+			std::cout << mx[i][j] << "\t";
+		std::cout << "\n";
+	}
+
+	clear_mx_memory(mx, n);
+	std::cout << std::endl;
+	*/
+
+	// Задача 2. Объединение массивов
+	/*
+	std::cout << "Задача 2.\nМассив №1:\n";
+	const int s2_1 = 5;
+	int arr2_1[s2_1];
+	fill(arr2_1, s2_1, -10, 15);
+	show(arr2_1, s2_1);
+
+	std::cout << "Массив №2:\n";
+	const int s2_2 = 7;
+	int arr2_2[s2_2];
+	fill(arr2_2, s2_2, -15, 5);
+	show(arr2_2, s2_2);
+
+	std::cout << "Итоговый массив:\n";
+	int* arr2_3 = pos_mem(arr2_1, s2_1, arr2_2, s2_2, s2_3);
+	show(arr2_3, s2_3);
+	delete[] arr2_3;
+
+	std::cout << std::endl;
+	*/
+
+	// Задача 3. Название числа
+
+	std::cout << "Задача 3.\nВведите двузначное число -> ";
+	std::cin >> n;
+	if (number_name(n, "number.txt"))
+		std::cout << "Число записано в файл.\n\n";
+	else
+		std::cout << "Ошибка выполнения функции.\n\n";
+
+
+	return 0;
+}
